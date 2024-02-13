d07aacc4e292dac656d64a71f2765fb789666c72
Gogo677 <diegogo_go@hotmail.com>
2024-02-12 19:41:03 -0600

    ProductService added

---
+++ ConsoleProducts/Services/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleProducts.Models;
+using ConsoleProducts.Repositories;
+
+namespace ConsoleProducts.Services
+{
+    internal class ProductService
+    {
+        private IRepository<Product> _repository; 
+
+        public ProductService(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Product> GetProducts() => _repository.getAll();
+
+        public void addProduct(Product product)
+        {
+            _repository.add(product);
+        }
+        
+    }
+}
